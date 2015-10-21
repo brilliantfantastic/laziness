199b9ca339c465436bdb48362a5ba8dc4d5384ff
Jamie Wright <jamie@brilliantfantastic.com>
2015-10-20 21:38:50 -0400

    Add optional parameters to rtm.start

--- lib/laziness/api/rtm.rb
+++ lib/laziness/api/rtm.rb
@@ -1,8 +1,11 @@
 module Slack
   module API
     class RTM < Base
-      def start
-        response = request :get, 'rtm.start'#, user: id
+      def start(simple_latest=false, no_unreads=false)
+        parameters = {}
+        parameters[:simple_latest] = simple_latest if simple_latest
+        parameters[:no_unreads] = no_unreads if no_unreads
+        response = request :get, 'rtm.start', parameters
         Slack::Session.parse response
       end
     end
