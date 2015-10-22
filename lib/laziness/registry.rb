7538a63a97fd758ce439a060a3e4032b6a7fad46
Jamie Wright <jamie@brilliantfantastic.com>
2015-10-21 22:13:40 -0400

    Observer registry implementation

---
+++ lib/laziness/registry.rb
@@ -0,0 +1,21 @@
+module Slack
+  class Registry
+    attr_reader :observers
+
+    def initialize
+      @observers = []
+    end
+
+    def notify(topic)
+      observers.select { |o| o.topic == topic }.map(&:execute)
+    end
+
+    def register(topic=nil, observer=nil, func=:update, &blk)
+      observers << Observer.new(topic, observer, func, &blk)
+    end
+
+    def unregister(topic=nil, observer=nil, func=:update, &blk)
+      observers.delete Observer.new(topic, observer, func, &blk)
+    end
+  end
+end
