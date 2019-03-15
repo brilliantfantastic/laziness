1dffc7db74157915d82e216b14aec699b6c62a19
Jamie Wright <jamie@brilliantfantastic.com>
2019-03-15 12:00:54 -0400

    Initial looping through the pagination

--- lib/laziness/cursor.rb
+++ lib/laziness/cursor.rb
@@ -7,7 +7,31 @@ module Slack
     end
 
     def paginate(&blk)
-      blk.call(Pager.new(page)) if block_given?
+      responses = []
+      if block_given?
+        pager = Pager.new(page)
+        loop do
+          response = blk.call(pager)
+          responses << response
+
+          break unless has_cursor?(response)
+
+          pager = pager.next(next_cursor(response))
+        end
+      end
+      responses
+    end
+
+    private
+
+    def next_cursor(response)
+      response["response_metadata"]["next_cursor"] unless !has_cursor?(response)
+    end
+
+    def has_cursor?(response)
+      response.respond_to?(:[]) &&
+        !response["response_metadata"].nil? &&
+          !response["response_metadata"]["next_cursor"].nil?
     end
   end
 end
--- spec/laziness/cursor_spec.rb
+++ spec/laziness/cursor_spec.rb
@@ -8,7 +8,37 @@ describe Slack::Cursor do
   end
 
   describe '#paginate' do
+    let(:first_response) do
+      {
+        "ok" => true,
+        "channels" => [{
+          "id" => "C01"
+        }],
+        "response_metadata" => {
+          "next_cursor" => "onto-second-request"
+        }
+      }
+    end
+    let(:last_response) do
+      {
+        "ok" => true,
+        "channels" => [{
+          "id" => "C03"
+        }]
+      }
+    end
     let(:page) { { limit: 10 } }
+    let(:second_response) do
+      {
+        "ok" => true,
+        "channels" => [{
+          "id" => "C02"
+        }],
+        "response_metadata" => {
+          "next_cursor" => "onto-last-request"
+        }
+      }
+    end
 
     subject { described_class.new(page) }
 
@@ -18,6 +48,38 @@ describe Slack::Cursor do
 
     it 'yields a pager' do
       expect { |b| subject.paginate(&b) }.to yield_with_args(Slack::Pager)
+    end
+
+    it 'returns the results of each yield' do
+      expected = [first_response, second_response, last_response]
+      index = 0
+      responses = subject.paginate do |pager|
+        response = expected[index]
+        index += 1
+        response
+      end
+
+      expect(responses).to eq(expected)
+    end
+
+    it 'updates the cursor on the pager' do
+      expect_any_instance_of(Slack::Pager).to \
+        receive(:next).with("onto-last-request").and_call_original
+
+      responses = [second_response, last_response]
+      subject.paginate do |pager|
+        responses.shift
+      end
+    end
+
+    context 'with a too many requests error' do
+      context 'without a max retries' do
+        xit 'bubbles up the error'
+      end
+
+      context 'with the max retries reached' do
+        xit 'bubbles up the error'
+      end
     end
 
     context 'with a nil page' do
